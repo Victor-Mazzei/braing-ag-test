d250f7dd40b9e79b61928203fad5b8ad10521807
victor mota mazzei <mazzei92@gmail.com>
2024-01-31 13:36:49 -0300

    Add cors, csurf, api prefix

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('port');
-
+  
   const config = new DocumentBuilder()
   .setTitle(configService.get('api_title'))
   .setDescription(configService.get('api_description'))
@@ -29,7 +30,9 @@ async function bootstrap() {
       },
     })
   );
-
+  app.setGlobalPrefix('v1');
+  app.use(csurf());
+  app.enableCors();
   await app.listen(port);
 }
 bootstrap();
